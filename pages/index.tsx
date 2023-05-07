890fa2acfacce1202ecbe929aee4cfa6bcc4ba7e
akanandhu <akananthu11@gmail.com>
2023-05-07 13:56:27 +0530

    feat(): install dependencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    
+      <p className='bg-red-500 font-bold '>Hello</p>
+      <div className='bg-red-600'>
+        Hello
+      </div>
   </Layout>
 )
 
